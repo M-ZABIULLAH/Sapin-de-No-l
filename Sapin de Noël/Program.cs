f8bd2547ad127b753c2284374fb9c82075a88649
M-zabi2024 <zabee_zh111@yahoo.com>
2024-12-22 22:08:51 +0100

    Add project files.

---
+++ Sapin de Noël/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+    {
+        static void Main()
+        {
+            Console.Write("Entrez la hauteur du sapin: ");
+            int height = int.Parse(Console.ReadLine());
+
+        
+            for (int i = 1; i <= height; i++)
+            {
+               
+                Console.Write(new string(' ', height - i));
+             
+                Console.WriteLine(new string('*', 2 * i - 1));
+            }
+
+         
+            for (int i = 0; i < height / 3; i++)
+            {
+                Console.Write(new string(' ', height - 1));
+                Console.WriteLine("|||");
+            }
+        }
+    }
+
